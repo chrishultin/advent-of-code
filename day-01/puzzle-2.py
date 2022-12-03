69273ba5e49f38f1985210f15c72810a296802fc
Christopher <chris@chrishultin.io>
2022-12-03 09:37:52 -0700

    Day 1

---
+++ day-01/puzzle-2.py
@@ -0,0 +1,20 @@
+INPUT_FILE = 'input.txt'
+calorie_data = []
+current_highest_index = 0
+current_index = 0
+with open(INPUT_FILE, 'r') as calorie_file:
+    current_elf_calories = 0
+    for line in calorie_file.readlines():
+        stripped_line = line.strip()
+        if stripped_line != "":
+            current_elf_calories += int(stripped_line)
+        else:
+            calorie_data.append(current_elf_calories)
+            if current_elf_calories > calorie_data[current_highest_index]:
+                current_highest_index = current_index
+            current_index += 1
+            current_elf_calories = 0
+
+calorie_data.sort(reverse=True)
+print(calorie_data)
+print(calorie_data[0]+calorie_data[1]+calorie_data[2])
