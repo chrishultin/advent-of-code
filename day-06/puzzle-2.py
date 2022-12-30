c9893281a0650261786d2f23919ac96532fd650b
Christopher <chris@chrishultin.io>
2022-12-30 10:39:45 -0700

    Consolidating AOC repos

--- day-06/puzzle-2.py
+++
@@ -1,8 +0,0 @@
-INPUT_FILE = 'input.txt'
-
-with open(INPUT_FILE,'r') as input_file:
-    line = input_file.readline().strip()
-    for i in range(14, len(line)):
-        if len(set(line[i-14:i])) == 14:
-            print(i)
-            break
